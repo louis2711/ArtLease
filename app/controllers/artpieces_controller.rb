90b9427d7d79889e4d6406a0398a6da52ef6909f
Hughes Stollsteiner <h.stollsteiner@gmail.com>
2020-05-18 17:07:51 +0200

    added methods to artpiece controller

--- app/controllers/artpieces_controller.rb
+++ app/controllers/artpieces_controller.rb
@@ -1,2 +1,22 @@
 class ArtpiecesController < ApplicationController
+  def index
+    @artpieces = Artpiece.all
+  end
+
+  def new
+    @artpiece = Artpiece.new
+  end
+
+  def create
+    @artpiece = Artpiece.new(artpiece_params)
+    @artpiece.save
+    redirect_to artpiece_path(@artpiece)
+  end
+
+  private
+
+  def artpiece_params
+    params.require(:artpiece).permit(:title, :artist, :description, :price, :availability)
+  end
+
 end
