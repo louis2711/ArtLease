fd65cbe2a1bbb8d230bd4979457679307b2ac92a
Jeanne Legay <jeanne.legay@hotmail.fr>
2020-05-20 15:08:49 +0200

    Create 4 users and real description for the firsts artpieces

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,26 +18,35 @@ User.destroy_all
 puts "data base cleaned"
 
 puts "creating users"
-user1 = User.create(email: "louis@lewagon.fr", first_name: "louis", last_name: "fr", password: "testtesttest")
-user1.save!
+louis = User.create(email: "louis@lewagon.fr", first_name: "louis", last_name: "f", password: "testtest")
+louis.save!
+
+stan = User.create(email: "stan@lewagon.fr", first_name: "stan", last_name: "s", password: "testtest")
+stan.save!
+
+hugues = User.create(email: "hugues@lewagon.fr", first_name: "hugues", last_name: "s", password: "testtest")
+hugues.save!
+
+jeanne = User.create(email: "jeanne@lewagon.fr", first_name: "jeanne", last_name: "l", password: "testtest")
+jeanne.save!
 
 puts "creating artpieces"
-joconde = Artpiece.new(user: User.first, title: 'La Joconde', artist: 'Leonard De Vinci', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.', price: 140, availability: true)
+joconde = Artpiece.new(user: User.first, title: 'La Joconde', artist: 'Leonard De Vinci', description: 'Très beau portrait de Mona Lisa par De Vinci. Assez petit, vous pourrez le mettre où vous le souhaitez.', price: 140, availability: true)
 file = URI.open('https://images.unsplash.com/photo-1423742774270-6884aac775fa?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60')
 joconde.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 joconde.save!
 
-noces_cana = Artpiece.new(user: User.first, title: 'Noces de Cana', artist: 'Veronese', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.', price: 110, availability: true)
+noces_cana = Artpiece.new(user: User.first, title: 'Noces de Cana', artist: 'Veronese', description: 'Très ancien tableau, il est très grand donc il faut un mur suffisamment haut et large pour pouvoir le mettre', price: 110, availability: true)
 file = URI.open('https://aleteiafrench.files.wordpress.com/2018/04/web-veronese-wedding-at-cana-domaine-public.jpg?quality=100&strip=all&w=720')
 noces_cana.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 noces_cana.save!
 
-penseur = Artpiece.new(user: User.first, title: 'Le Penseur', artist: 'Auguste Rodin', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.', price: 80, availability: true)
+penseur = Artpiece.new(user: User.first, title: 'Le Penseur', artist: 'Auguste Rodin', description: 'Haute sculpture. Idéal pour un jardin', price: 80, availability: true)
 file = URI.open('https://www.museumtv.art/wp-content/uploads/2017/09/le-penseur-rodin-01.jpg')
 penseur.photo.attach(io: file, filename: 'noces_cana.png', content_type: 'image/png')
 penseur.save!
 
-nympheas = Artpiece.new(user: User.first, title: 'Les nymphéas', artist: 'Claude Monet', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.', price: 125, availability: true)
+nympheas = Artpiece.new(user: User.first, title: 'Les nymphéas', artist: 'Claude Monet', description: 'Idéal pour mettre dans une pièce lumineuse. Locations longue durée de préférence', price: 125, availability: true)
 file = URI.open('https://cdn.pariscityvision.com/library/image/5624.jpg')
 nympheas.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 nympheas.save!
@@ -47,24 +56,24 @@ file = URI.open('https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Van_G
 nuit_etoilee.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 nuit_etoilee.save!
 
-american_gothic = Artpiece.new(user: User.first, title: 'American Gothic', artist: 'Grant Wood', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.', price: 75, availability: true)
+american_gothic = Artpiece.new(user: User.first, title: 'American Gothic', artist: 'Grant Wood', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.', price: 75, availability: false)
 file = URI.open('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cc/Grant_Wood_-_American_Gothic_-_Google_Art_Project.jpg/260px-Grant_Wood_-_American_Gothic_-_Google_Art_Project.jpg')
 american_gothic.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 american_gothic.save!
 
-guernica = Artpiece.new(user: User.first, title: 'Guernica', artist: 'Pablo Picasso', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.', price: 200, availability: true)
+guernica = Artpiece.new(user: User.first, title: 'Guernica', artist: 'Pablo Picasso', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.', price: 200, availability: false)
 file = URI.open('http://dp.mariottini.free.fr/weekend/madrid/photo/guernica.jpg')
 guernica.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 guernica.save!
 
 
-promenade = Artpiece.new(user: User.first, title: 'La Promenade', artist: 'Claude Monet', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.', price: 175, availability: true)
+promenade = Artpiece.new(user: User.first, title: 'La Promenade', artist: 'Claude Monet', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.', price: 175, availability: false)
 file = URI.open('https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Monet_Umbrella.JPG/1200px-Monet_Umbrella.JPG')
 promenade.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 promenade.save!
 
 
-argenteuil = Artpiece.new(user: User.first, title: 'Argenteuil', artist: 'Edouard Manet', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.', price: 185, availability: true)
+argenteuil = Artpiece.new(user: User.first, title: 'Argenteuil', artist: 'Edouard Manet', description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit.', price: 185, availability: false)
 file = URI.open('https://upload.wikimedia.org/wikipedia/commons/thumb/8/8d/Edouard_Manet_003.jpg/260px-Edouard_Manet_003.jpg')
 argenteuil.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
 argenteuil.save!
