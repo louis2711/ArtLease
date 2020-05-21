c4fb048f4e63b54912595402d1c0cb08a0564d5d
Louis Fresnel <louisfresnel@live.fr>
2020-05-21 16:37:33 +0200

    modal

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,15 +10,16 @@ class BookingsController < ApplicationController
   def create
     @booking = Booking.new(booking_params)
     @booking.user = current_user
+    @booking.artpiece_id = params[:artpiece_id]
     @booking.status = "waiting"
     @booking.save!
-    redirect_to booking_path(@artpiece)
+    redirect_to artpiece_path(@booking.artpiece)
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date)
+    params.require(:booking).permit(:start_date, :end_date, :create)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,14 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :artpieces, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :artpieces, only: [:index, :new, :create, :edit, :update, :destroy] do
+  resources :bookings, only: [:index, :create]
+  end
 
   get "artpieces/:id", to: "artpieces#show"
   # , as: :artpiece
 
-  resources :bookings, only: [:index, :create]
+
 
   get 'dashboard', to:'pages#dashboard'
 
